5c5bf860dcedcda076f84ad083b9af3f8135d96f
Ciacco Davide <CiaccoDavide@users.noreply.github.com>
2015-10-28 16:32:17 +0100

    Setup

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject camera = new GameObject();
+        camera.transform.position = new Vector3(0, 0, -10);
+        camera.AddComponent<Camera>();
+        camera.name = "Camera";
+        GameObject player = new GameObject();
+        Texture2D texture = Resources.Load("Textures/doge") as Texture2D;
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width,texture.height), new Vector2(0, 0));
+        SpriteRenderer sp = player.AddComponent<SpriteRenderer>();
+        sp.sprite = sprite;
+        player.name = "Player";
+        player.transform.position = new Vector2(-sp.bounds.size.x / 2.0f, -sp.bounds.size.y / 2.0f);
+  	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Tetrogue
+*Tetrogue is a rogue-like/rpg game blended with tetris.*
+
+**Collaborators**:
+ - Ciacco Davide - *Team leader and procedural-thinker*
+ - Ceffa Filippo - *Hardcore programmer and yellow-glassed wiseman*
+ 
+-
+
+**TO DO**
+ - Coding
+ - Music
+ - Graphics
