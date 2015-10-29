01b97edac9b519fb7f606540bb73d121591465f7
CiaccoDavide <davideciacco@cln-233-149.eduroam.unimi.it>
2015-10-29 16:01:49 +0100

    loadGrounds implemented
    loadGrounds loads the ground texture and types data

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 public class Main : MonoBehaviour {
 
+
+    private Ground temp_ground;
+    private string terrain_list = "./Assets/Resources/Data/terrains.txt";
+    private string temp_string;
+
 	// Use this for initialization
 	void Start () {
+        loadGrounds();
+
         GameObject camera = new GameObject();
         camera.transform.position = new Vector3(0, 0, -10);
         camera.AddComponent<Camera>();
@@ -18,5 +27,18 @@ public class Main : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 	}
+
+    void loadGrounds(){
+        List<Ground> grounds = new List<Ground>();
+        using (StreamReader r = new StreamReader(terrain_list)){
+            while ((temp_string = r.ReadLine()) != null){
+                string[] substrings = temp_string.Split(',');
+                temp_ground=new Ground(substrings[0],float.Parse(substrings[1]));
+                grounds.Add(temp_ground);
+                //print(" Path: "+substrings[0]+" | Speed: "+float.Parse(substrings[1]));
+            }
+        }
+    }
 }
